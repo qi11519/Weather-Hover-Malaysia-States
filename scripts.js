7095bd1d4e00e00fbae6eaa71cc095943b846849
qi11519 <qi11519@gmail.com>
2023-09-27 19:48:25 +0800

    V2
    - Transformed JavaScript code to jQuery.
    - Resolved layout issue.
    - Fixed various functionality bugs.

--- scripts.js
+++ scripts.js
@@ -1,42 +1,39 @@
-function showForecast(name) {
-    const thebox = document.getElementById('thebox');
-    const statename = document.getElementById('statename');
-
-    thebox.style.visibility="visible";
-    thebox.style.opacity="1";
-
-    statename.textContent = name;
-    
-    if (name == "Johor" ){
-        var locationID = "1";
-    } else if (name == "Kedah"){
-        var locationID = "2";
-    } else if (name == "Kelantan"){
-        var locationID = "3";
-    } else if (name == "Melaka"){
-        var locationID = "6";
-    } else if (name == "Negeri Sembilan"){
-        var locationID = "7";
-    } else if (name == "Pahang"){
-        var locationID = "8";
-    } else if (name == "Pulau Pinang"){
-        var locationID = "9";
-    } else if (name == "Perak"){
-        var locationID = "10";
-    } else if (name == "Perlis"){
-        var locationID = "11";
-    } else if (name == "Selangor"){
-        var locationID = "15";
-    } else if (name == "Terengganu"){
-        var locationID = "16";
-    } else if (name == "Sabah"){
-        var locationID = "13";
-    } else if (name == "Sarawak"){
-        var locationID = "14";
-    } else if (name == "Labuan"){
-        var locationID = "5";
+const options = {
+    method : "GET",
+    mode: 'cors',
+    headers: {
+        Authorization: "METToken 63d724fba9f9323723b5ae28166a1f2db586747c",
+        //SAMPLE TOKEN FOR MET API (Recommended to put in .env)
+        //METToken 84e08247cef2678af77f5f10c4fca798fc1a1a7a
+        //METToken 63d724fba9f9323723b5ae28166a1f2db586747c
     }
-    
+};
+
+function showForecast(name) {
+    const infoBox = $('#infoBox'); 
+    const statename = $('#statename');
+
+    infoBox.css({visibility: "visible", opacity: "1"});
+
+    statename.text(name);
+
+    const locationMap = {
+        "Johor": "1",
+        "Kedah": "2",
+        "Kelantan": "3",
+        "Labuan": "5",
+        "Melaka": "6",
+        "Negeri Sembilan": "7",
+        "Pahang": "8",
+        "Pulau Pinang": "9",
+        "Perak": "10",
+        "Perlis": "11",
+        "Sabah": "13",
+        "Sarawak": "14",
+        "Selangor": "15",
+        "Terengganu": "16"
+    };
+
     var dateTime = new Date();
 
     var year = dateTime.getFullYear();
@@ -53,64 +50,59 @@ function showForecast(name) {
 
     var today = year+'-'+month+'-'+day;
 
-    var theUrl ="https://api.met.gov.my/v2/data?datasetid=FORECAST&datacategoryid=GENERAL&locationid=LOCATION:"+locationID+"&start_date="+today+"&end_date="+today;
+    var theUrl ="https://api.met.gov.my/v2/data?datasetid=FORECAST&datacategoryid=GENERAL&locationid=LOCATION:"+locationMap[name]+"&start_date="+today+"&end_date="+today;
 
     httpGetAsync(theUrl);
-    //"https://api.met.gov.my/v2/data?datasetid=FORECAST&datacategoryid=GENERAL&locationid=LOCATION:237&start_date=2022-04-28&end_date=2022-04-28"
+    //Example: "https://api.met.gov.my/v2/data?datasetid=FORECAST&datacategoryid=GENERAL&locationid=LOCATION:237&start_date=2022-04-28&end_date=2022-04-28"
 }
   
 function hideForecast() {
-    const thebox = document.getElementById('thebox');
+    const infoBox = $('#infoBox');
 
-    thebox.style.visibility="hidden";
-    thebox.style.opacity="0";
+    infoBox.css({visibility: "hidden", opacity: "0"})
 }
 
 function httpGetAsync(theUrl){
 
-    //var theUrl = "https://api.met.gov.my/v2.1/locations?locationcategoryid=TOWN&offset=0";
+    //MET API SAMPLE LINK: var theUrl = "https://api.met.gov.my/v2.1/locations?locationcategoryid=TOWN&offset=0";
 
-    const options = {
-        method : "GET",
-        mode: 'cors',
-        headers: {
-            Authorization: "METToken 63d724fba9f9323723b5ae28166a1f2db586747c",
-            //METToken 84e08247cef2678af77f5f10c4fca798fc1a1a7a
-            //METToken 63d724fba9f9323723b5ae28166a1f2db586747c
+    $.ajax({
+        url: theUrl,
+        type: options.method,
+        headers: options.headers,
+        success: function (data) {
+            updateInfo(data);
+        },
+        error: function (error) {
+            console.error('Error:', error);
         }
-    };
-    
-    fetch(theUrl, options)
-    .then(( res ) => {
-        return res.json();
-    })
-    .then( (data) => updateInfo(data));
+    });
 }
 
 function updateInfo(data){
-    const FGM = document.getElementById('FGM');
-    const FGA = document.getElementById('FGA');
-    const FGN = document.getElementById('FGN');
-    const FMAXT = document.getElementById('fmaxt');
-    const FMINT = document.getElementById('fmint');
-    const FSIGW = document.getElementById('fsigw');
-    const SIGW = document.getElementById('fsigwTime');
+    const FGM = $('#FGM');
+    const FGA = $('#FGA');
+    const FGN = $('#FGN');
+    const FMAXT = $('#fmaxt');
+    const FMINT = $('#fmint');
+    const FSIGW = $('#fsigw');
+    const SIGW = $('#fsigwTime');
 
     for (var i=0; i<data.results.length; i++) {
         for (var key in data.results[i]) {
             if (data.results[i].datatype == "FGM") {
-                FGM.textContent = data.results[i].value;
+                FGM.text(data.results[i].value);
             } else if (data.results[i].datatype == "FGA") {
-                FGA.textContent = data.results[i].value;
+                FGA.text(data.results[i].value);
             } else if (data.results[i].datatype == "FGN") {
-                FGN.textContent = data.results[i].value;
+                FGN.text(data.results[i].value);
             } else if (data.results[i].datatype == "FMAXT") {
-                FMAXT.textContent = data.results[i].value;
+                FMAXT.text(data.results[i].value);
             } else if (data.results[i].datatype == "FMINT") {
-                FMINT.textContent = data.results[i].value;
+                FMINT.text(data.results[i].value);
             } else if (data.results[i].datatype == "FSIGW") {
-                FSIGW.textContent = data.results[i].value;
-                SIGW.textContent = data.results[i].attributes.when;
+                FSIGW.text(data.results[i].value);
+                SIGW.text(data.results[i].attributes.when);
             }
         }
      }
@@ -123,7 +115,7 @@ function displayTime(){
     var sec = dateTime.getSeconds();
     var date = dateTime.getFullYear()+' / '+(dateTime.getMonth()+1)+' / '+dateTime.getDate()+"     -     ";
     var date2 = dateTime.getFullYear()+' / '+(dateTime.getMonth()+1)+' / '+dateTime.getDate();
-    var session = document.getElementById('session');
+    var session = $('#session');
 
     if(hrs >= 12){
         session.innerHTML = 'PM';
@@ -147,11 +139,11 @@ function displayTime(){
         sec = "0"+sec;
     }
 
-    document.getElementById('hours').innerHTML = hrs;
-    document.getElementById('minutes').innerHTML = min;
-    document.getElementById('seconds').innerHTML = sec;
-    document.getElementById('day').innerHTML = date;
-    document.getElementById('gifTime').innerHTML = date2;
+    $('#hours').text(hrs);
+    $('#minutes').text(min);
+    $('#seconds').text(sec);
+    $('#day').text(date);
+    $('#gifTime').text(date2);
 }
 
 function openTab(evt, functionName) {
@@ -167,7 +159,6 @@ function openTab(evt, functionName) {
 
     
     if (month == 1 || month == 3 ||month == 5 || month == 7 || month == 8 || month == 10|| month == 12){
-        console.log("odd");
         if (day1 == 29){
             day3 = 1;
             month3 = month3 + 1;
@@ -201,25 +192,18 @@ function openTab(evt, functionName) {
     var day2nd = year+'-'+month2+'-'+day2;
     var day3rd = year+'-'+month3+'-'+day3;
 
-    const today = document.getElementById('day1')
-    const tomorrow = document.getElementById('day2')
-    const tomorrow2 = document.getElementById('day3')
+    const today = $('#day1')
+    const tomorrow = $('#day2')
+    const tomorrow2 = $('#day3')
 
-    today.textContent = day1st;
-    tomorrow.textContent = day2nd;
-    tomorrow2.textContent = day3rd;
+    today.text(day1st);
+    tomorrow.text(day2nd);
+    tomorrow2.text(day3rd);
 
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(functionName).style.display = "block";
-    evt.currentTarget.className += " active";
+    $(".tabcontent").css("display", "none");
+    $(".tablinks").removeClass("active");
+    $("#" + functionName).css("display", "block");
+    $(evt.currentTarget).addClass("active");
 }
 
 function searchLocation(){
@@ -230,16 +214,6 @@ function searchLocation(){
     for (var i = 0; i < 151; i=i+50){
 
         var townUrl = "https://api.met.gov.my/v2.1/locations?locationcategoryid=TOWN&offset=" + i;
-
-        const options = {
-            method : "GET",
-            mode: 'cors',
-            headers: {
-                Authorization: "METToken 63d724fba9f9323723b5ae28166a1f2db586747c",
-                //METToken 84e08247cef2678af77f5f10c4fca798fc1a1a7a
-                //METToken 63d724fba9f9323723b5ae28166a1f2db586747c
-            }
-        };
 
         fetch(townUrl, options)
         .then(( res ) => {
@@ -280,21 +254,20 @@ function searchLocation(){
 function find(data){
 
     var locationID;
-    var theLocation = document.querySelector(".search-bar").value;
+    var theLocation = $(".search-bar").val();
 
-    for (var i=0; i<data.results.length; i++) {
-        for (var key in data.results[i]) {
-            if (data.results[i].name == theLocation) {
-                
-                locationID = data.results[i].id;
-                const locationName = document.getElementById('foundLocation');
+    $.each(data.results, function(i, item) {
+        if (item.name == theLocation.toUpperCase()) {
 
-                locationName.textContent = data.results[i].name;
+            locationID = item.id;
+            const locationName = $('#foundLocation');
+            
+            locationName.text(item.name);
 
-                break;
-            } 
+            return false;
         }
-    }
+    });
+
     return locationID;
 }
 
@@ -343,29 +316,32 @@ function updateLocationInfo(data){
     var day2nd = year+'-'+month2+'-'+day2;
     var day3rd = year+'-'+month3+'-'+day3;
 
-    const FGM1 = document.getElementById('locationFGM1');
-    const FGA1 = document.getElementById('locationFGA1');
-    const FGN1 = document.getElementById('locationFGN1');
-    const FMAXT1 = document.getElementById('locationfmaxt1');
-    const FMINT1 = document.getElementById('locationfmint1');
-    const FSIGW1 = document.getElementById('locationfsigw1');
-    const SIGW1 = document.getElementById('locationfsigwTime1');
+    //FIRST DAY
+    const FGM1 = $('#locationFGM1');
+    const FGA1 = $('#locationFGA1');
+    const FGN1 = $('#locationFGN1');
+    const FMAXT1 = $('#locationfmaxt1');
+    const FMINT1 = $('#locationfmint1');
+    const FSIGW1 = $('#locationfsigw1');
+    const SIGW1 = $('#locationfsigwTime1');
 
-    const FGM2 = document.getElementById('locationFGM2');
-    const FGA2 = document.getElementById('locationFGA2');
-    const FGN2 = document.getElementById('locationFGN2');
-    const FMAXT2 = document.getElementById('locationfmaxt2');
-    const FMINT2 = document.getElementById('locationfmint2');
-    const FSIGW2 = document.getElementById('locationfsigw2');
-    const SIGW2 = document.getElementById('locationfsigwTime2');
+    //SECOND DAY
+    const FGM2 = $('#locationFGM2');
+    const FGA2 = $('#locationFGA2');
+    const FGN2 = $('#locationFGN2');
+    const FMAXT2 = $('#locationfmaxt2');
+    const FMINT2 = $('#locationfmint2');
+    const FSIGW2 = $('#locationfsigw2');
+    const SIGW2 = $('#locationfsigwTime2');
 
-    const FGM3 = document.getElementById('locationFGM3');
-    const FGA3 = document.getElementById('locationFGA3');
-    const FGN3 = document.getElementById('locationFGN3');
-    const FMAXT3 = document.getElementById('locationfmaxt3');
-    const FMINT3 = document.getElementById('locationfmint3');
-    const FSIGW3 = document.getElementById('locationfsigw3');
-    const SIGW3 = document.getElementById('locationfsigwTime3');
+    //THIRD DAY
+    const FGM3 = $('#locationFGM3');
+    const FGA3 = $('#locationFGA3');
+    const FGN3 = $('#locationFGN3');
+    const FMAXT3 = $('#locationfmaxt3');
+    const FMINT3 = $('#locationfmint3');
+    const FSIGW3 = $('#locationfsigw3');
+    const SIGW3 = $('#locationfsigwTime3');
 
     for (var i=0; i<data.results.length; i++) {
         for (var key in data.results[i]) {
@@ -373,48 +349,48 @@ function updateLocationInfo(data){
             dataDate = data.results[i].date.slice(0,10);
             if (dataDate == day1st){
                 if (data.results[i].datatype == "FGM") {
-                    FGM1.textContent = data.results[i].value;
+                    FGM1.text(data.results[i].value);
                 } else if (data.results[i].datatype == "FGA") {
-                    FGA1.textContent = data.results[i].value;
+                    FGA1.text(data.results[i].value);
                 } else if (data.results[i].datatype == "FGN") {
-                    FGN1.textContent = data.results[i].value;
+                    FGN1.text(data.results[i].value);
                 } else if (data.results[i].datatype == "FMAXT") {
-                    FMAXT1.textContent = data.results[i].value;
+                    FMAXT1.text(data.results[i].value);
                 } else if (data.results[i].datatype == "FMINT") {
-                    FMINT1.textContent = data.results[i].value;
+                    FMINT1.text(data.results[i].value);
                 } else if (data.results[i].datatype == "FSIGW") {
-                    FSIGW1.textContent = data.results[i].value;
-                    SIGW1.textContent = data.results[i].attributes.when;
+                    FSIGW1.text(data.results[i].value);
+                    SIGW1.text(data.results[i].attributes.when);
                 }
             } else if (dataDate == day2nd){
                 if (data.results[i].datatype == "FGM") {
-                    FGM2.textContent = data.results[i].value;
+                    FGM2.text(data.results[i].value);
                 } else if (data.results[i].datatype == "FGA") {
-                    FGA2.textContent = data.results[i].value;
+                    FGA2.text(data.results[i].value);
                 } else if (data.results[i].datatype == "FGN") {
-                    FGN2.textContent = data.results[i].value;
+                    FGN2.text(data.results[i].value);
                 } else if (data.results[i].datatype == "FMAXT") {
-                    FMAXT2.textContent = data.results[i].value;
+                    FMAXT2.text(data.results[i].value);
                 } else if (data.results[i].datatype == "FMINT") {
-                    FMINT2.textContent = data.results[i].value;
+                    FMINT2.text(data.results[i].value);
                 } else if (data.results[i].datatype == "FSIGW") {
-                    FSIGW2.textContent = data.results[i].value;
-                    SIGW2.textContent = data.results[i].attributes.when;
+                    FSIGW2.text(data.results[i].value);
+                    SIGW2.text(data.results[i].attributes.when);
                 }
             } else if (dataDate == day3rd){
                 if (data.results[i].datatype == "FGM") {
-                    FGM3.textContent = data.results[i].value;
+                    FGM3.text(data.results[i].value);
                 } else if (data.results[i].datatype == "FGA") {
-                    FGA3.textContent = data.results[i].value;
+                    FGA3.text(data.results[i].value);
                 } else if (data.results[i].datatype == "FGN") {
-                    FGN3.textContent = data.results[i].value;
+                    FGN3.text(data.results[i].value);
                 } else if (data.results[i].datatype == "FMAXT") {
-                    FMAXT3.textContent = data.results[i].value;
+                    FMAXT3.text(data.results[i].value);
                 } else if (data.results[i].datatype == "FMINT") {
-                    FMINT3.textContent = data.results[i].value;
+                    FMINT3.text(data.results[i].value);
                 } else if (data.results[i].datatype == "FSIGW") {
-                    FSIGW3.textContent = data.results[i].value;
-                    SIGW3.textContent = data.results[i].attributes.when;
+                    FSIGW3.text(data.results[i].value);
+                    SIGW3.text(data.results[i].attributes.when);
                 }
             }
         }
@@ -449,19 +425,11 @@ function httpGetAsyncForLocation(locationID){
 
     var day3rd = year+'-'+month3+'-'+day3;
 
-    //var locationID = document.getElementById('foundID').textContent;
+    //var locationID = $('#foundID').textContent;
     var theUrl ="https://api.met.gov.my/v2/data?datasetid=FORECAST&datacategoryid=GENERAL&locationid="+locationID+"&start_date="+today+"&end_date="+day3rd;
-                //https://api.met.gov.my/v2/data?datasetid=FORECAST&datacategoryid=GENERAL&locationid=LOCATION:"+locationID+"&start_date="+today+"&end_date="+today;
+                //Sample: https://api.met.gov.my/v2/data?datasetid=FORECAST&datacategoryid=GENERAL&locationid=LOCATION:"+locationID+"&start_date="+today+"&end_date="+today;
 
-    const options = {
-        method : "GET",
-        mode: 'cors',
-        headers: {
-            Authorization: "METToken 84e08247cef2678af77f5f10c4fca798fc1a1a7a",
-            //METToken 84e08247cef2678af77f5f10c4fca798fc1a1a7a
-            //METToken 63d724fba9f9323723b5ae28166a1f2db586747c
-        }
-    };
+    $.ajax
     
     fetch(theUrl, options)
     .then(( res ) => {
@@ -472,13 +440,26 @@ function httpGetAsyncForLocation(locationID){
         return res.json();
     })
     .then( (data) => updateLocationInfo(data));
+
+    $.ajax({
+        url: theUrl,
+        type: options.method,
+        headers: options.headers,
+        success: function (data) {
+            updateLocationInfo(data);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            if (jqXHR.status == 404) {
+                alert('No Forecast Data for current location.');
+            } else {
+                alert('An error occurred: ' + errorThrown);
+            }
+        }
+    });
 }
 
-window.onload = function () {
-    start();
-};
-
-function start() {
-    setInterval(displayTime, 10);
-    document.getElementById("defaultOpen").click();
-}
+$(document).ready(function() {
+    $("#defaultOpen").trigger("click");
+    displayTime();
+    setInterval(displayTime, 1000);
+});
